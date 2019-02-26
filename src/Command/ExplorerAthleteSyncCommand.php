59035c9b99f2bc73333e9bbeaccdf32379a571fe
Maciej Zgadzaj <maciej.zgadzaj@gmail.com>
2019-02-26 10:41:12 +0100

    Command explorer:athlete:sync - handle exceptions

--- src/Command/ExplorerAthleteSyncCommand.php
+++ src/Command/ExplorerAthleteSyncCommand.php
@@ -154,109 +154,112 @@ class ExplorerAthleteSyncCommand extends ContainerAwareCommand
      */
     private function syncRoutes(Athlete $athlete)
     {
-        $message = 'Syncing routes for <comment>%athlete_name%</comment> (%athlete_id%).';
-        $params = [
-            '%athlete_name%' => $athlete->getName(),
-            '%athlete_id%' => $athlete->getId(),
-        ];
-        $this->logger->debug(strtr(strip_tags($message), $params));
-        $this->output->writeln(strtr($message, $params));
+        try {
+            $message = 'Syncing routes for <comment>%athlete_name%</comment> (%athlete_id%).';
+            $params = [
+                '%athlete_name%' => $athlete->getName(),
+                '%athlete_id%'   => $athlete->getId(),
+            ];
+            $this->logger->debug(strtr(strip_tags($message), $params));
+            $this->output->writeln(strtr($message, $params));
 
-        if (empty($this->stravaService->getAthleteAccessToken($athlete))) {
-            $message = '- <error>No Strava access token found for athlete.</error>';
-            $this->logger->error(strip_tags($message));
-            $this->output->writeln($message);
-            return;
-        }
+            if (empty($this->stravaService->getAthleteAccessToken($athlete))) {
+                $message = '- <error>No Strava access token found for athlete.</error>';
+                $this->logger->error(strip_tags($message));
+                $this->output->writeln($message);
+                return;
+            }
 
-        $this->output->writeln('- Fetching routes from Strava...');
-        $stravaRoutes = $this->stravaService->getAthleteRoutes($athlete);
+            $this->output->writeln('- Fetching routes from Strava...');
+            $stravaRoutes = $this->stravaService->getAthleteRoutes($athlete);
 
-        $message = '- Routes fetched: <comment>%count%</comment>';
-        $params = [
-            '%count%' => count($stravaRoutes),
-        ];
-        $this->logger->debug(strtr(strip_tags($message), $params));
-        $this->output->writeln(strtr($message, $params));
+            $message = '- Routes fetched: <comment>%count%</comment>';
+            $params = [
+                '%count%' => count($stravaRoutes),
+            ];
+            $this->logger->debug(strtr(strip_tags($message), $params));
+            $this->output->writeln(strtr($message, $params));
 
-        if (empty($stravaRoutes)) {
-            $message = '- <error>No routes to sync.</error>';
-            $this->logger->debug(strip_tags($message));
-            $this->output->writeln($message);
-            return;
-        }
+            if (empty($stravaRoutes)) {
+                $message = '- <error>No routes to sync.</error>';
+                $this->logger->debug(strip_tags($message));
+                $this->output->writeln($message);
+                return;
+            }
 
-        $localRoutes = $this->routeService->getAthleteRoutes($athlete);
+            $localRoutes = $this->routeService->getAthleteRoutes($athlete);
 
-        $message = '- Local routes found: <comment>%count%</comment>';
-        $params = [
-            '%count%' => count($localRoutes),
-        ];
-        $this->logger->debug(strtr(strip_tags($message), $params));
-        $this->output->writeln(strtr($message, $params));
+            $message = '- Local routes found: <comment>%count%</comment>';
+            $params = [
+                '%count%' => count($localRoutes),
+            ];
+            $this->logger->debug(strtr(strip_tags($message), $params));
+            $this->output->writeln(strtr($message, $params));
 
-        $publicAdded = $publicUpdated = $publicStarred = $publicDeleted = $published = $privateSkipped = $privateDeleted = 0;
-        $syncedIds = [];
+            $publicAdded = $publicUpdated = $publicStarred = $publicDeleted = $published = $privateSkipped = $privateDeleted = 0;
+            $syncedIds = [];
 
-        foreach ($stravaRoutes as $routeData) {
-            if (!$routeData->private) {
-                // If a local route already exists, re-use its current public value.
-                $localRoute = $this->routeService->load($routeData->id);
-                $routeData->public = $localRoute ? $localRoute->isPublic() : true;
-                $syncedIds[] = $routeData->id;
+            foreach ($stravaRoutes as $routeData) {
+                if (!$routeData->private) {
+                    // If a local route already exists, re-use its current public value.
+                    $localRoute = $this->routeService->load($routeData->id);
+                    $routeData->public = $localRoute ? $localRoute->isPublic() : TRUE;
+                    $syncedIds[] = $routeData->id;
 
-                if ($routeData->public) {
-                    $published++;
-                }
+                    if ($routeData->public) {
+                        $published++;
+                    }
 
-                // Create or update route.
-                $route = $this->routeService->save($routeData);
+                    // Create or update route.
+                    $route = $this->routeService->save($routeData);
 
-                // If route belongs to a different athlete, and public is set to false
-                // by current athlete, let's just not add starred_by value.
-                if ($route->getAthlete()->getId() != $athlete->getId() && $routeData->public) {
-                    if (!$route->isStarredBy($athlete)) {
-                        $message = '- Starred new route: <info>%route_name%</info> (%route_id%)';
+                    // If route belongs to a different athlete, and public is set to false
+                    // by current athlete, let's just not add starred_by value.
+                    if ($route->getAthlete()->getId() != $athlete->getId() && $routeData->public) {
+                        if (!$route->isStarredBy($athlete)) {
+                            $message = '- Starred new route: <info>%route_name%</info> (%route_id%)';
+                            $params = [
+                                '%route_name%' => $route->getName(),
+                                '%route_id%'   => $route->getId(),
+                            ];
+                            $this->logger->info(strtr(strip_tags($message), $params));
+                            $this->output->writeln(strtr($message, $params));
+                        }
+                        $route->addStarredBy($athlete);
+                        $publicStarred++;
+                        $syncedIds[] = $routeData->id;
+                    }
+
+                    if ($route->isNew()) {
+                        $message = '- Added new route: <info>%route_name%</info> (%route_id%)';
                         $params = [
                             '%route_name%' => $route->getName(),
-                            '%route_id%' => $route->getId(),
+                            '%route_id%'   => $route->getId(),
                         ];
                         $this->logger->info(strtr(strip_tags($message), $params));
                         $this->output->writeln(strtr($message, $params));
+                        $publicAdded++;
                     }
-                    $route->addStarredBy($athlete);
-                    $publicStarred++;
-                    $syncedIds[] = $routeData->id;
+                    else {
+                        $publicUpdated++;
+                    }
                 }
-
-                if ($route->isNew()) {
-                    $message = '- Added new route: <info>%route_name%</info> (%route_id%)';
-                    $params = [
-                        '%route_name%' => $route->getName(),
-                        '%route_id%' => $route->getId(),
-                    ];
-                    $this->logger->info(strtr(strip_tags($message), $params));
-                    $this->output->writeln(strtr($message, $params));
-                    $publicAdded++;
-                } else {
-                    $publicUpdated++;
+                else {
+                    if ($this->routeService->exists($routeData->id)) {
+                        $this->routeService->delete($routeData->id);
+                        $privateDeleted++;
+                    }
+                    $privateSkipped++;
                 }
-            } else {
-                if ($this->routeService->exists($routeData->id)) {
-                    $this->routeService->delete($routeData->id);
-                    $privateDeleted++;
-                }
-                $privateSkipped++;
             }
-        }
 
-        $publicDeleted = $this->routeService->deleteAthleteRoutes($athlete, $syncedIds);
-        $unstarred = $this->routeService->unstarAthleteRoutes($athlete, $syncedIds);
+            $publicDeleted = $this->routeService->deleteAthleteRoutes($athlete, $syncedIds);
+            $unstarred = $this->routeService->unstarAthleteRoutes($athlete, $syncedIds);
 
-        $athlete->setLastSync(new \DateTime());
-        $this->entityManager->flush();
+            $athlete->setLastSync(new \DateTime());
+            $this->entityManager->flush();
 
-        $message = '- Routes synced: 
+            $message = '- Routes synced: 
   - public added: <info>%public_added%</info>
   - public updated: <info>%public_updated%</info>
   - public starred: <info>%public_starred%</info> 
@@ -265,17 +268,22 @@ class ExplorerAthleteSyncCommand extends ContainerAwareCommand
   - published: <info>%published%</info> 
   - private skipped: <info>%private_skipped%</info>
   - private deleted: <info>%private_deleted%</info>';
-        $params = [
-            '%public_added%' => $publicAdded,
-            '%public_updated%' => $publicUpdated,
-            '%public_starred%' => $publicStarred,
-            '%unstarred%' => $unstarred,
-            '%public_deleted%' => $publicDeleted,
-            '%published%' => $published,
-            '%private_skipped%' => $privateSkipped,
-            '%private_deleted%' => $privateDeleted,
-        ];
-        $this->logger->debug(strtr(strip_tags($message), $params));
-        $this->output->writeln(strtr($message, $params));
+            $params = [
+                '%public_added%'    => $publicAdded,
+                '%public_updated%'  => $publicUpdated,
+                '%public_starred%'  => $publicStarred,
+                '%unstarred%'       => $unstarred,
+                '%public_deleted%'  => $publicDeleted,
+                '%published%'       => $published,
+                '%private_skipped%' => $privateSkipped,
+                '%private_deleted%' => $privateDeleted,
+            ];
+            $this->logger->debug(strtr(strip_tags($message), $params));
+            $this->output->writeln(strtr($message, $params));
+        }
+        catch (\Exception $e) {
+            $this->logger->debug($e->getMessage());
+            $this->output->writeln($e->getMessage());
+        }
     }
 }
